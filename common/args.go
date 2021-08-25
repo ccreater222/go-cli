ae6e01efe9720bd32f72c324abffe539b8137310
ccreater <1126774947@qq.com>
2021-08-25 21:33:15 +0800

    delete useless code

--- common/args.go
+++ common/args.go
@@ -1,24 +1,8 @@
 package common
 
 import (
-	"fmt"
 	"github.com/alexflint/go-arg"
-	"reflect"
 )
-func MakeStruct(vals ...interface{}) interface{} {
-	var sfs []reflect.StructField
-	for k, v := range vals {
-		t := reflect.TypeOf(v)
-		sf := reflect.StructField{
-			Name: fmt.Sprintf("F%d", (k + 1)),
-			Type: t,
-		}
-		sfs = append(sfs, sf)
-	}
-	st := reflect.StructOf(sfs)
-	so := reflect.New(st)
-	return so.Interface()
-}
 
 func Args() * arg.Parser  {
 
